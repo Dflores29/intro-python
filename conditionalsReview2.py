a81ecf3d7f3916429450521b174e4f8fc5014ab9
Flores <dflores107@cps.k12.il.us>
2024-05-06 12:12:53 -0500

    whileLoops

--- conditionalsReview2.py
+++ conditionalsReview2.py
@@ -2,29 +2,55 @@
 # Ask the user to input a score. Assume the score is out of 100.
 
 
-#functions allow me to wrap up code and reuse it 
-def scoreMe():
-    score = int(input("Enter your score: "))
-    print("You entered", score)
-    # Implement the logic to determine the grade based on the score:
-    # A score of 90 and above is an "A".
-    if score >= 90:
-        print("A")
-    # A score of 80 to 89 is a "B".
-    elif score >= 80 and score <90: 
-        print("B")
-    # A score of 70 to 79 is a "C".
-    elif score >= 70 and score < 80:
-        print("C")
-    # A score of 60 to 69 is a "D".
-    elif score >= 60 and score < 70:
-        print("D")
-    # A score below 60 is an "F".
-    elif score >= 50 and score < 60:
-        print("F")
-    # Ensure the score entered is within the valid range (0-100). If not, print an error message.
+# #functions allow me to wrap up code and reuse it 
+# def scoreMe():
+#     score = int(input("Enter your score: "))
+#     print("You entered", score)
+#     # Implement the logic to determine the grade based on the score:
+#     # A score of 90 and above is an "A".
+#     if score >= 90:
+#         print("A")
+#     # A score of 80 to 89 is a "B".
+#     elif score >= 80 and score <90: 
+#         print("B")
+#     # A score of 70 to 79 is a "C".
+#     elif score >= 70 and score < 80:
+#         print("C")
+#     # A score of 60 to 69 is a "D".
+#     elif score >= 60 and score < 70:
+#         print("D")
+#     # A score below 60 is an "F".
+#     elif score >= 50 and score < 60:
+#         print("F")
+#     # Ensure the score entered is within the valid range (0-100). If not, print an error message.
     
-#call the function
-scoreMe()
-scoreMe()
-scoreMe()
+# #call the function
+# scoreMe()
+# scoreMe()
+# scoreMe()
+
+dietaryrestrictions = input("Do you have any dietary restrictions?")
+if dietaryrestrictions == "yes":
+    type = (input("What kind?"))
+else:
+    print("Thank you.")
+if type == "Vegetarian":
+    print("You can eat a salad.")
+elif type == "gluten-free":
+    print("You can eat gluten-free pasta")
+elif type == "nut-free":
+    print("You can eat a burger")
+meal = input("What meal would you like? ")
+print("The meal fits your preference")
+payment = (input("Do you have a payment plan? "))
+if payment == "yes":
+    print("its been successfully paid.")
+else:
+    amount = int(input("Enter the amount of payment "))
+
+outside = input("would you like to go outside? ")
+outside = True
+if outside:
+    print("You can go eat at the picnic table.")
+else:
+    print("Stay inside")
---
+++ whileLoops.py
@@ -0,0 +1,26 @@
+# nums = [1, 2, 3, 4, 5]
+
+# # nested for loop
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+
+# #Ranges
+# for i in range(1, 11):
+#     print(i)
+
+#wild loops
+x = 0
+
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+
+# while True:
+#     print("Enter your name: ")
+#     name = input()
+#     if name == "your name":
+#         print("your name is" + name)
+#         break
